c7c31d0bff99968d446b1bba36f3427fb697224f
Caroline Toebben <54775655+Calorine@users.noreply.github.com>
2021-02-11 19:29:26 +0100

    added messages_de.dart for localization

---
+++ lib/generated/intl/messages_de.dart
@@ -0,0 +1,111 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a de locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'de';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static _notInlinedMessages(_) => <String, Function> {
+    "add" : MessageLookupByLibrary.simpleMessage("Hinzufügen"),
+    "addDevice" : MessageLookupByLibrary.simpleMessage("Gerät hinzufügen"),
+    "addDeviceInstructionText" : MessageLookupByLibrary.simpleMessage("1.) Stecken Sie beide PLC-Geräte in die gewünschten Wandsteckdosen und warten ca. 45 Sekunden.\n2.) Drücken Sie kurz den Verschlüsselungsknopf des ersten (evtl. bereits vorhandenen) PLC-Gerätes.\n    (Alternativ kann das Pairing auch über das Webinterface des bereits vorhandenen Geräts gestartet werden.) \n3.) Drücken Sie innerhalb von zwei Minuten den Verschlüsselungsknopf des zweiten (neuen) PLC-Gerätes ebenfalls kurz.\n4.) Sobald die LEDs dauerhaft leuchten, sind die PLC-Geräte betriebsbereit."),
+    "appColor" : MessageLookupByLibrary.simpleMessage("App Farben"),
+    "appInfo" : MessageLookupByLibrary.simpleMessage("App Info"),
+    "appInformation" : MessageLookupByLibrary.simpleMessage("App Information"),
+    "appearance" : MessageLookupByLibrary.simpleMessage("Erscheinungbild"),
+    "attachedToRouter" : MessageLookupByLibrary.simpleMessage("An Router angeschlossen:"),
+    "back" : MessageLookupByLibrary.simpleMessage("zurück"),
+    "cancel" : MessageLookupByLibrary.simpleMessage("Abbrechen"),
+    "change" : MessageLookupByLibrary.simpleMessage("ändern"),
+    "changeNetworkview" : MessageLookupByLibrary.simpleMessage("Ändere Netzwerkansicht"),
+    "changePlcnetworkPassword" : MessageLookupByLibrary.simpleMessage("PLC-Netzwerk Passwort ändern"),
+    "changeTheLanguageOfTheApp" : MessageLookupByLibrary.simpleMessage("Ändere die Sprache der App"),
+    "checkUpdates" : MessageLookupByLibrary.simpleMessage(" Updates suchen "),
+    "chooseMainColorAccentColorAndFontColors" : MessageLookupByLibrary.simpleMessage("Wähle Hauptfarbe, Akzentfarbe und Schriftfarben"),
+    "chooseTheme" : MessageLookupByLibrary.simpleMessage("Wähle Thema"),
+    "cockpitSoftwareIsUpToDate" : MessageLookupByLibrary.simpleMessage("Cockpit Software auf dem neusten Stand."),
+    "confirm" : MessageLookupByLibrary.simpleMessage("Bestätigen"),
+    "contactInfo" : MessageLookupByLibrary.simpleMessage("Kontakt info"),
+    "currentVersion" : MessageLookupByLibrary.simpleMessage("aktuelle Version"),
+    "dataRatesArePermanentlyDisplayedInTheOverview" : MessageLookupByLibrary.simpleMessage("Datenraten werden dauerhaft in der Übersicht angezeigt "),
+    "deleteDevice" : MessageLookupByLibrary.simpleMessage("Gerät \nlöschen"),
+    "deviceinfo" : MessageLookupByLibrary.simpleMessage("Geräteinfo"),
+    "enableShowingSpeeds" : MessageLookupByLibrary.simpleMessage("Anzeigen der Datenraten"),
+    "factoryReset" : MessageLookupByLibrary.simpleMessage("Werkseinstellung \nsetzen"),
+    "fontsize" : MessageLookupByLibrary.simpleMessage("Schriftgröße"),
+    "forward" : MessageLookupByLibrary.simpleMessage("weiter"),
+    "fullyCustomizeColors" : MessageLookupByLibrary.simpleMessage("Vollständig anpassbare Farben"),
+    "generateSupportInformation" : MessageLookupByLibrary.simpleMessage("Support Informationen generieren "),
+    "homeNetworkDesktop" : MessageLookupByLibrary.simpleMessage("Home Netzwerk Desktop"),
+    "identifyDevice" : MessageLookupByLibrary.simpleMessage("Gerät \nidentifizieren"),
+    "ignoreUpdates" : MessageLookupByLibrary.simpleMessage("Ignoriere zukünftige Aktualisierungen (Updates)"),
+    "install" : MessageLookupByLibrary.simpleMessage("Installieren"),
+    "internetcentered" : MessageLookupByLibrary.simpleMessage("Internetzentrisch"),
+    "ipaddress" : MessageLookupByLibrary.simpleMessage("IP-Adresse:"),
+    "isLocalDevice" : MessageLookupByLibrary.simpleMessage("Ist lokales Gerät:"),
+    "language" : MessageLookupByLibrary.simpleMessage("Sprache"),
+    "launchWebinterface" : MessageLookupByLibrary.simpleMessage("Webinterface \nöffnen "),
+    "macaddress" : MessageLookupByLibrary.simpleMessage("MAC-Adresse:"),
+    "mtnumber" : MessageLookupByLibrary.simpleMessage("MT-Nummer:"),
+    "name" : MessageLookupByLibrary.simpleMessage("Name"),
+    "network" : MessageLookupByLibrary.simpleMessage("Netzwerk"),
+    "networkSettings" : MessageLookupByLibrary.simpleMessage("Netzwerk Einstellungen"),
+    "networkoverview" : MessageLookupByLibrary.simpleMessage("Netzwerkübersicht"),
+    "newVersion" : MessageLookupByLibrary.simpleMessage("neue Version"),
+    "nextNetwork" : MessageLookupByLibrary.simpleMessage("nächstes Netzwerk"),
+    "noDevicesFoundNscanningForDevices" : MessageLookupByLibrary.simpleMessage("No devices found \nScanning for devices ..."),
+    "openBrowser" : MessageLookupByLibrary.simpleMessage("öffne browser"),
+    "openOptimization" : MessageLookupByLibrary.simpleMessage("Optimierungshilfe öffnen"),
+    "openZip" : MessageLookupByLibrary.simpleMessage("öffne zip"),
+    "optimizationHelp" : MessageLookupByLibrary.simpleMessage("Optimierungs Hilfe"),
+    "otherDevicesEgPcAreDisplayedInTheOverview" : MessageLookupByLibrary.simpleMessage("Weitere Geräte, z.B. PC werden in der Übersicht angezeigt"),
+    "overview" : MessageLookupByLibrary.simpleMessage("Übersicht"),
+    "pleaseConfirmAction" : MessageLookupByLibrary.simpleMessage("Bitte Aktion bestätigen."),
+    "pleaseConfirmActionAttentionYourRouterIsConnectedToThis" : MessageLookupByLibrary.simpleMessage("Bitte Aktion bestätigen. \nAchtung! Ihr Router ist an dieses PLC-Gerät angeschlossen sie verlieren die Verbiondung zum Internet "),
+    "pleaseEnterDevicename" : MessageLookupByLibrary.simpleMessage("Bitte Gerätenamen eintragen"),
+    "pleaseEnterPassword" : MessageLookupByLibrary.simpleMessage("Bitte Passwort eingeben"),
+    "pleaseEnterProcessingNumber" : MessageLookupByLibrary.simpleMessage("Bitte Bearbeitungsnummer eintragen"),
+    "pleaseEnterYourMailAddress" : MessageLookupByLibrary.simpleMessage("Bitte tragen sie ihre E-mail Adresse ein"),
+    "pleaseFillInYourName" : MessageLookupByLibrary.simpleMessage("Bitte tagen sie ihren Namen ein"),
+    "previousNetwork" : MessageLookupByLibrary.simpleMessage("vorheriges Netzwerk"),
+    "processNumber" : MessageLookupByLibrary.simpleMessage("Bearbeitungsnummer"),
+    "recordTheTransmissionPowerOfTheDevicesAndTransmitIt" : MessageLookupByLibrary.simpleMessage("Übertragungsleistung der Geräte Aufzeichnen und an devolo übermitteln"),
+    "refresh" : MessageLookupByLibrary.simpleMessage("neu laden"),
+    "sendToDevolo" : MessageLookupByLibrary.simpleMessage("sende an devolo"),
+    "serialNumber" : MessageLookupByLibrary.simpleMessage("Seriennummer:"),
+    "set" : MessageLookupByLibrary.simpleMessage("setzen"),
+    "settings" : MessageLookupByLibrary.simpleMessage("Einstellungen"),
+    "showLogs" : MessageLookupByLibrary.simpleMessage("Zeige Logs"),
+    "showManual" : MessageLookupByLibrary.simpleMessage("Handbuch \nanzeigen"),
+    "showOtherDevices" : MessageLookupByLibrary.simpleMessage("Anzeigen anderer Geräte"),
+    "showPassword" : MessageLookupByLibrary.simpleMessage("Passwort anzeigen "),
+    "state" : MessageLookupByLibrary.simpleMessage("Status"),
+    "support" : MessageLookupByLibrary.simpleMessage("Support"),
+    "theCreatedSupportInformationCanNowBeSentToDevolo" : MessageLookupByLibrary.simpleMessage("Die erstellten Support-Informationen können jetzt zum devolo Support gesendet werden.\\nBitte füllen sie folgende Felder aus."),
+    "theOverviewWillBeCenteredAroundThePlcDeviceConnected" : MessageLookupByLibrary.simpleMessage("Die Übersicht wird um das PLC-Gerät zentriert, welches am Internet angeschlossen ist"),
+    "title" : MessageLookupByLibrary.simpleMessage("devolo Cockpit"),
+    "type" : MessageLookupByLibrary.simpleMessage("Typ:"),
+    "update" : MessageLookupByLibrary.simpleMessage("Aktualisierung"),
+    "updateReadyToInstall" : MessageLookupByLibrary.simpleMessage("Update bereit zur Installation."),
+    "updates" : MessageLookupByLibrary.simpleMessage("Aktualisierungen"),
+    "version" : MessageLookupByLibrary.simpleMessage("Version:"),
+    "windowsNetworkThrottling" : MessageLookupByLibrary.simpleMessage("Windows Netzwerkdrosselung"),
+    "yourEmailaddress" : MessageLookupByLibrary.simpleMessage(" Ihre E-mail Adresse"),
+    "yourName" : MessageLookupByLibrary.simpleMessage("Ihr Name")
+  };
+}

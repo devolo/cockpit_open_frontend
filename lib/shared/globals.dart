64a6baf738d7f4715f350eb088297de74c909690
Molkenbur <thomas.molkenbur@devolo.de>
2021-05-06 00:42:20 +0200

    Correct version format

--- lib/shared/globals.dart
+++ lib/shared/globals.dart
@@ -1,4 +1,4 @@
 // defines the version, is a combination of the cockpit backend version (against which the flutter frontend was developed) and the date of the release build
 // format: <developBackendVersion>_YYYY-MM-DD
-String version_frontend = "5.1.6_2021_05_05";
+String version_frontend = "5.1.6_2021-05-05";
 String version_backend = "5.1.6.2"; // ToDo get current installed backend version from backend

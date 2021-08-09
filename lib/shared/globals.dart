c33345915587451679830df113dd7d04961257e4
Krishna Subramanian <krishna.subramanian@devolo.de>
2021-08-09 14:55:33 +0200

    Add configuration for device simulator

--- lib/shared/globals.dart
+++ lib/shared/globals.dart
@@ -2,3 +2,7 @@
 // format: <developBackendVersion>_YYYY-MM-DD
 String versionFrontend = "5.1.6_2021-05-05";
 String versionBackend = "5.1.6.2"; // ToDo get current installed backend version from backend
+
+// Define config options for device simulator
+const bool simulatedDevices = true;
+const int nDevices = 5;
